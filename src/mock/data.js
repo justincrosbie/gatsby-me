043d72e01bb64bb60f39c2f215f74594982cf314
justincrosbie <justincrosbie@gmail.com>
2023-04-25 22:02:34 +1000

    Projects updates

--- src/mock/data.js
+++ src/mock/data.js
@@ -32,18 +32,17 @@ export const projectsData = [
     img: 'project.jpg',
     title: 'This site!',
     info: 'This is my personal website, its written in Gatsby and React, using a standard template I found on the interwebs. ',
-    info2: 'Can\'t wait to hook it up to Chat-GPT for some interesting experiments. Hmmm.....',
+    info2: 'Who knows what I will do next with it....',
     url: 'https://github.com/justincrosbie/gatsby-me'
   },
   {
     id: nanoid(),
     img: 'project.jpg',
     title: 'Stuff I do at work',
-    info: 'I won\t go into detail here, for fear if violating DRY principles ;) But I have developed many full-stack web apps, mostly in Angular with Java or Node REST backends',
-    info2: 'These days, while I am still heavily involved in development, most of my responsibilities are centred around ensuring the team and the product are successful, and the customers are happy :)',
+    info: 'I won\t go into detail here, for fear of violating DRY principles ;) Check out my resume for details!',
+    info2: 'But I have developed many full-stack web apps, mostly in Angular with Java or Node REST backends\n\nThese days, while I am still heavily involved in development, most of my responsibilities are centred around ensuring the team and the product are successful, and the customers are happy :)',
     info3: 'Mostly this is around ensuring the team are using best Agile practices (this is an art in itself, and not easy to get right!), Story writing (also another art - if you get this right, that is half the battle, if you get it - meh, almost right - you are plunged into the abyss of misaligned objectives and perpetual scope creep!!)',
-    info4: 'Oh and tests. Trust me, you didn\'t write enough tests!',
-    info5: 'Check out my resume for details!',
+    info4: 'Oh and tests. You didn\'t write enough tests!',
     url: ''
   },
   {
