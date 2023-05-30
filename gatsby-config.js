ed1c4ce520d20d487647d7dcaa684df1f940fdf6
justincrosbie <justincrosbie@gmail.com>
2023-05-30 23:12:46 +1000

    Adds Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,5 +31,23 @@ module.exports = {
         icon: 'src/images/favicon.png',
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-0VBFLVPCB6", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
+      },
+    },
   ],
 };
